e554900e8fe36101fdd0785428aaadba38b82ca2
Tommy Brown <46692709+tbrowndev@users.noreply.github.com>
2020-07-20 23:10:25 -0500

    added www folder for web

---
+++ www/search-tab-search-module-es5.js
@@ -0,0 +1,231 @@
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["search-tab-search-module"], {
+  /***/
+  "./node_modules/raw-loader/dist/cjs.js!./src/app/search-tab/search.page.html":
+  /*!***********************************************************************************!*\
+    !*** ./node_modules/raw-loader/dist/cjs.js!./src/app/search-tab/search.page.html ***!
+    \***********************************************************************************/
+
+  /*! exports provided: default */
+
+  /***/
+  function node_modulesRawLoaderDistCjsJsSrcAppSearchTabSearchPageHtml(module, __webpack_exports__, __webpack_require__) {
+    "use strict";
+
+    __webpack_require__.r(__webpack_exports__);
+    /* harmony default export */
+
+
+    __webpack_exports__["default"] = "<ion-header [translucent]=\"true\">\n  <ion-toolbar>\n    <ion-title>\n      Search\n    </ion-title>\n  </ion-toolbar>\n</ion-header>\n\n<ion-content [fullscreen]=\"true\">\n  <ion-header collapse=\"condense\">\n    <ion-toolbar>\n      <ion-title size=\"large\">Search</ion-title>\n    </ion-toolbar>\n  </ion-header>\n\n  <app-explore-container name=\"Search Page\"></app-explore-container>\n</ion-content>\n";
+    /***/
+  },
+
+  /***/
+  "./src/app/search-tab/search-routing.module.ts":
+  /*!*****************************************************!*\
+    !*** ./src/app/search-tab/search-routing.module.ts ***!
+    \*****************************************************/
+
+  /*! exports provided: SearchPageRoutingModule */
+
+  /***/
+  function srcAppSearchTabSearchRoutingModuleTs(module, __webpack_exports__, __webpack_require__) {
+    "use strict";
+
+    __webpack_require__.r(__webpack_exports__);
+    /* harmony export (binding) */
+
+
+    __webpack_require__.d(__webpack_exports__, "SearchPageRoutingModule", function () {
+      return SearchPageRoutingModule;
+    });
+    /* harmony import */
+
+
+    var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(
+    /*! tslib */
+    "./node_modules/tslib/tslib.es6.js");
+    /* harmony import */
+
+
+    var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(
+    /*! @angular/core */
+    "./node_modules/@angular/core/__ivy_ngcc__/fesm2015/core.js");
+    /* harmony import */
+
+
+    var _angular_router__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(
+    /*! @angular/router */
+    "./node_modules/@angular/router/__ivy_ngcc__/fesm2015/router.js");
+    /* harmony import */
+
+
+    var _search_page__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(
+    /*! ./search.page */
+    "./src/app/search-tab/search.page.ts");
+
+    var routes = [{
+      path: '',
+      component: _search_page__WEBPACK_IMPORTED_MODULE_3__["SearchPage"]
+    }];
+
+    var SearchPageRoutingModule = function SearchPageRoutingModule() {
+      _classCallCheck(this, SearchPageRoutingModule);
+    };
+
+    SearchPageRoutingModule = Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"])([Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+      imports: [_angular_router__WEBPACK_IMPORTED_MODULE_2__["RouterModule"].forChild(routes)],
+      exports: [_angular_router__WEBPACK_IMPORTED_MODULE_2__["RouterModule"]]
+    })], SearchPageRoutingModule);
+    /***/
+  },
+
+  /***/
+  "./src/app/search-tab/search.module.ts":
+  /*!*********************************************!*\
+    !*** ./src/app/search-tab/search.module.ts ***!
+    \*********************************************/
+
+  /*! exports provided: SearchPageModule */
+
+  /***/
+  function srcAppSearchTabSearchModuleTs(module, __webpack_exports__, __webpack_require__) {
+    "use strict";
+
+    __webpack_require__.r(__webpack_exports__);
+    /* harmony export (binding) */
+
+
+    __webpack_require__.d(__webpack_exports__, "SearchPageModule", function () {
+      return SearchPageModule;
+    });
+    /* harmony import */
+
+
+    var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(
+    /*! tslib */
+    "./node_modules/tslib/tslib.es6.js");
+    /* harmony import */
+
+
+    var _ionic_angular__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(
+    /*! @ionic/angular */
+    "./node_modules/@ionic/angular/__ivy_ngcc__/fesm2015/ionic-angular.js");
+    /* harmony import */
+
+
+    var _angular_core__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(
+    /*! @angular/core */
+    "./node_modules/@angular/core/__ivy_ngcc__/fesm2015/core.js");
+    /* harmony import */
+
+
+    var _angular_common__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(
+    /*! @angular/common */
+    "./node_modules/@angular/common/__ivy_ngcc__/fesm2015/common.js");
+    /* harmony import */
+
+
+    var _angular_forms__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(
+    /*! @angular/forms */
+    "./node_modules/@angular/forms/__ivy_ngcc__/fesm2015/forms.js");
+    /* harmony import */
+
+
+    var _search_page__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(
+    /*! ./search.page */
+    "./src/app/search-tab/search.page.ts");
+    /* harmony import */
+
+
+    var _explore_container_explore_container_module__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(
+    /*! ../explore-container/explore-container.module */
+    "./src/app/explore-container/explore-container.module.ts");
+    /* harmony import */
+
+
+    var _search_routing_module__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(
+    /*! ./search-routing.module */
+    "./src/app/search-tab/search-routing.module.ts");
+
+    var SearchPageModule = function SearchPageModule() {
+      _classCallCheck(this, SearchPageModule);
+    };
+
+    SearchPageModule = Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"])([Object(_angular_core__WEBPACK_IMPORTED_MODULE_2__["NgModule"])({
+      imports: [_ionic_angular__WEBPACK_IMPORTED_MODULE_1__["IonicModule"], _angular_common__WEBPACK_IMPORTED_MODULE_3__["CommonModule"], _angular_forms__WEBPACK_IMPORTED_MODULE_4__["FormsModule"], _explore_container_explore_container_module__WEBPACK_IMPORTED_MODULE_6__["ExploreContainerComponentModule"], _search_routing_module__WEBPACK_IMPORTED_MODULE_7__["SearchPageRoutingModule"]],
+      declarations: [_search_page__WEBPACK_IMPORTED_MODULE_5__["SearchPage"]]
+    })], SearchPageModule);
+    /***/
+  },
+
+  /***/
+  "./src/app/search-tab/search.page.scss":
+  /*!*********************************************!*\
+    !*** ./src/app/search-tab/search.page.scss ***!
+    \*********************************************/
+
+  /*! exports provided: default */
+
+  /***/
+  function srcAppSearchTabSearchPageScss(module, __webpack_exports__, __webpack_require__) {
+    "use strict";
+
+    __webpack_require__.r(__webpack_exports__);
+    /* harmony default export */
+
+
+    __webpack_exports__["default"] = "\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzcmMvYXBwL3NlYXJjaC10YWIvc2VhcmNoLnBhZ2Uuc2NzcyJ9 */";
+    /***/
+  },
+
+  /***/
+  "./src/app/search-tab/search.page.ts":
+  /*!*******************************************!*\
+    !*** ./src/app/search-tab/search.page.ts ***!
+    \*******************************************/
+
+  /*! exports provided: SearchPage */
+
+  /***/
+  function srcAppSearchTabSearchPageTs(module, __webpack_exports__, __webpack_require__) {
+    "use strict";
+
+    __webpack_require__.r(__webpack_exports__);
+    /* harmony export (binding) */
+
+
+    __webpack_require__.d(__webpack_exports__, "SearchPage", function () {
+      return SearchPage;
+    });
+    /* harmony import */
+
+
+    var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(
+    /*! tslib */
+    "./node_modules/tslib/tslib.es6.js");
+    /* harmony import */
+
+
+    var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(
+    /*! @angular/core */
+    "./node_modules/@angular/core/__ivy_ngcc__/fesm2015/core.js");
+
+    var SearchPage = function SearchPage() {
+      _classCallCheck(this, SearchPage);
+    };
+
+    SearchPage = Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"])([Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Component"])({
+      selector: 'app-search',
+      template: Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__importDefault"])(__webpack_require__(
+      /*! raw-loader!./search.page.html */
+      "./node_modules/raw-loader/dist/cjs.js!./src/app/search-tab/search.page.html"))["default"],
+      styles: [Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__importDefault"])(__webpack_require__(
+      /*! ./search.page.scss */
+      "./src/app/search-tab/search.page.scss"))["default"]]
+    })], SearchPage);
+    /***/
+  }
+}]);
+//# sourceMappingURL=search-tab-search-module-es5.js.map
