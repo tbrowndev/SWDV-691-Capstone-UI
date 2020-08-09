7f3042842e3cd3ab18ad1ac2db02be2e93965b27
Tommy Brown <46692709+tbrowndev@users.noreply.github.com>
2020-08-09 06:22:50 -0500

    added logic to count likes and comments as well as like and unlike post

--- src/pages/post/post.ts
+++ src/pages/post/post.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import { Post, Comment, Subcomment } from '../../objects/objectFactory'
+import { Post, Comment } from '../../objects/objectFactory'
 import { GroupPage } from '../group/group'
 import { Group_DataProvider } from '../../service/service';
 import { Share } from '../../service/share';
@@ -14,25 +14,63 @@ export class PostPage {
   post: Post;
   comments: Comment[] = [];
   postReply: string = "";
+  likeCount: number = 0;
+  commentCount: number = 0;
+  memberLiked: number = 0;
+  likeId:number;
 
   constructor(public navCtrl: NavController, public navPar: NavParams, public groupService: Group_DataProvider, public shared: Share) {
     this.post = navPar.get('selectedPost');
-
     this.getPostComments(this.post.id);
+    this.getLikeCount();
+    this.getCommentCount();
   }
 
-  getPostLikes(post:Post){
-    // this.groupService.count_post_likes(post.id, this.shared.items["userId"]).subscribe(
-    //   res =>{
-    //     if(res.status == 200){
-    //       //return [res.likes, res.memberLiked]
-    //     }
-    //   }
-    // )
+  getLikeCount() {
+    this.groupService.count_post_likes(this.post.id, this.shared.items["userId"]).subscribe(
+      res => {
+        if (res.status == 200) {
+          this.likeCount = res.likes;
+          this.memberLiked = res.memberLiked;
+          this.likeId = res.likeId;
+        }
+      }
+    )
   }
 
-  likePost(post:Post){
+  getCommentCount() {
+    this.groupService.count_post_comments(this.post.id).subscribe(
+      res => {
+        if (res.status == 200) {
+          this.commentCount = res.count;
+        }
+      }
+    )
+  }
 
+  likePost() {
+    if (this.memberLiked == 1) {
+      //go to database and delete like
+      this.groupService.delete_like_from_post(this.likeId).subscribe(
+        res => {
+          if (res.status == 200){
+            this.memberLiked = 0;
+            this.getLikeCount();
+          }
+        }
+      )
+    }
+    else {
+      //go to database and add like
+      this.groupService.add_like_to_post(this.post.id, this.post.groupId, this.shared.items["userId"]).subscribe(
+        res => {
+          if (res.status == 200){
+            this.memberLiked = 1;
+            this.getLikeCount();
+          }
+        }
+      )
+    }
   }
 
   replyToPost() {
@@ -42,6 +80,7 @@ export class PostPage {
           this.postReply = "";
           this.shared.presentToast(res.message);
           this.getPostComments(this.post.id);
+          this.getCommentCount();
         }
       });
     }
@@ -54,20 +93,20 @@ export class PostPage {
     )
   }
 
-  continueComments(infiniteScroll) {
-    setTimeout(() => {
-      //Go to server and retrieve comments associated with post
+  // continueComments(infiniteScroll) {
+  //   setTimeout(() => {
+  //     //Go to server and retrieve comments associated with post
 
-      infiniteScroll.complete();
-    }, 500);
-  }
+  //     infiniteScroll.complete();
+  //   }, 500);
+  // }
 
-  getSubComments(id: number) {
-    let subComments: Subcomment[] = [];
-    //Go to server and retrieve comments associated with post
+  // getSubComments(id: number) {
+  //   let subComments: Subcomment[] = [];
+  //   //Go to server and retrieve comments associated with post
 
-    return subComments;
-  }
+  //   return subComments;
+  // }
 
   showGroup() {
     this.navCtrl.push(GroupPage, { "id": this.post.groupId })
