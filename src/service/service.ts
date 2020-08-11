cb8d39fe80fe93b24c193544c8a5ae0fb1078b07
Tommy Brown <46692709+tbrowndev@users.noreply.github.com>
2020-08-10 22:27:38 -0500

    updated base url for services

--- src/service/service.ts
+++ src/service/service.ts
@@ -29,7 +29,7 @@ function handle_error(error: Response | any) {
 @Injectable()
 export class Auth_DataProvider {
 
-    baseURL = 'http://localhost:6200';
+    baseURL = 'ec2-3-19-75-69.us-east-2.compute.amazonaws.com:6200';
 
     dataChanged$: Observable<boolean>;
     private dataChangeSubject: Subject<boolean>;
@@ -61,7 +61,7 @@ export class Auth_DataProvider {
 @Injectable()
 export class User_DataProvider {
 
-    baseURL = 'http://localhost:6220';
+    baseURL = 'ec2-3-19-75-69.us-east-2.compute.amazonaws.com:6220';
 
     dataChanged$: Observable<boolean>;
     private dataChangeSubject: Subject<boolean>;
@@ -98,7 +98,7 @@ export class User_DataProvider {
 @Injectable()
 export class Group_DataProvider {
 
-    baseURL = 'http://localhost:6210';
+    baseURL = 'ec2-3-19-75-69.us-east-2.compute.amazonaws.com:6210';
 
     dataChanged$: Observable<boolean>;
     private dataChangeSubject: Subject<boolean>;
